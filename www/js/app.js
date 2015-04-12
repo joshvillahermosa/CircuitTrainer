8d286de540007b61138c21bb178de63817425adb
joshvillahermosa <joshvillahermosa@gmail.com>
2015-04-12 16:36:42 -0400

    [Dev] Temp fix on routing

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('circuit', ['ionic', 'circuit.controllers', 'circuit.services'])
+angular.module('circuit', ['ionic', 'circuit.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
