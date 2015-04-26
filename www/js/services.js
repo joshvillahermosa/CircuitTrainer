dc9e7594c41bfae93b0a312093e1f6256ff7a9ee
joshvillahermosa <joshvillahermosa@gmail.com>
2015-04-26 17:31:28 -0400

    [dev] Service to listen to timer start events

--- www/js/services.js
+++ www/js/services.js
@@ -6,4 +6,21 @@ angular.module('circuit.services', [])
       return $http.get('exercises/'+exercise+'/index.json');
     }
   };
+})
+
+.service('timerStartCheck', function(){
+  var isStart;
+
+  var setIsStart = function(didStart){
+    isStart = didStart;
+  };
+
+  var getIsStart = function() {
+    return isStart;
+  };
+
+  return {
+    setIsStart: setIsStart,
+    getIsStart: getIsStart
+  }
 });
